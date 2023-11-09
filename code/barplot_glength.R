bcc04b8bc9f9666e01c0d5bf80e8f099d30bd4e0
lukenoel20 <luke.noel@uconn.edu>
2023-11-09 18:26:58 -0500

    updated paths in barplot script

--- code/barplot_glength.R
+++ code/barplot_glength.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(ggplot2)
 library(viridis)
 
-g_times <- read.csv("../data/game_times.csv")
+g_times <- read.csv("course-paper-or-presentation-lukenoel20/data/game_times.csv")
 
 # Converting AvgTime into a decimal
 g_times <- g_times %>%
@@ -17,11 +17,11 @@ g_times$decimal_time <- g_times$hours + g_times$minutes / 60
 g_times$Year <- as.factor(g_times$Year)
 
 g_length <- ggplot(g_times, aes(x = Year, y = decimal_time, fill = Year)) +
-  geom_bar(stat = "identity", position = "identity", color = "white", size = 1.5) +
+  geom_bar(stat = "identity", position = "identity", color = "white", linewidth = 1.5) +
   scale_fill_viridis(discrete = TRUE, option = "viridis") +
   labs(x = "Year",
        y = "Average Length of Game (Hours)") +
   theme_minimal() +
   theme(legend.position = "none", axis.text.x = element_text(angle = 45, hjust = 1))
 
-ggsave("barplot_glength.pdf", g_length, height=4, width=6)
+ggsave("barplot_glength.pdf", g_length, path="course-paper-or-presentation-lukenoel20/manuscript", height=4.2, width=6)
